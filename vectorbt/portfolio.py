d606352f666abdafff9c294d9c9d95cf0f0ecca9
Oleg Polakow <olegpolakow@gmail.com>
2020-01-27 00:35:50 +0100

    Add support for 2-dim processing in portfolio

--- vectorbt/portfolio.py
+++ vectorbt/portfolio.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from vectorbt.utils.array import *
+from vectorbt.utils.array import fshift, pct_change, ffill
 from vectorbt.timeseries import TimeSeries
 from vectorbt.positions import Positions
 
@@ -14,13 +14,9 @@ class Portfolio():
             raise TypeError("Argument ts is not TimeSeries")
         if not isinstance(positions, Positions):
             raise TypeError("Argument positions is not Positions")
-        if isinstance(slippage, np.ndarray) and not isinstance(slippage, TimeSeries):
-            raise TypeError("Argument slippage is not TimeSeries")
         # Safety check whether index of ts and positions is the same
         if not np.array_equal(ts.index, positions.index):
             raise ValueError("Arguments ts and positions do not share the same index")
-        if isinstance(slippage, np.ndarray) and not np.array_equal(ts.index, slippage.index):
-            raise ValueError("Arguments ts and slippage do not share the same index")
 
         self.ts = ts
         self.positions = positions
@@ -32,8 +28,8 @@ class Portfolio():
     def equity(self):
         """Calculate running equity."""
         # Calculate equity
-        pos_idx = np.flatnonzero(self.positions)
-        equity = np.ones(len(self.positions))
+        pos_idx = np.nonzero(self.positions)
+        equity = np.ones(self.positions.shape)
         returns_mask = fshift(ffill(self.positions), 1) == 1
         equity[returns_mask] += pct_change(self.ts)[returns_mask]
         # Apply fees and slippage
@@ -41,31 +37,24 @@ class Portfolio():
             equity[pos_idx] *= 1 - self.slippage[pos_idx]
         else:
             equity[pos_idx] *= 1 - self.slippage
-        equity[pos_idx] *= 1 - self.fees
-        equity = np.cumprod(equity)
-        # NaN before first position
-        equity[:pos_idx[0]] = 1
-        return TimeSeries(equity, index=self.ts.index)
+        if isinstance(self.fees, np.ndarray):
+            equity[pos_idx] *= 1 - self.fees[pos_idx]
+        else:
+            equity[pos_idx] *= 1 - self.fees
+        equity = np.cumprod(equity, axis=0)
+        return TimeSeries(equity, index=self.ts.index, columns=self.ts.columns)
 
     @property
     def cash_equity(self):
         """Calculate running equity in cash."""
-        return TimeSeries(self.equity * self.investment, index=self.ts.index)
+        return self.equity * self.investment
 
     @property
     def share_equity(self):
         """Calculate running equity in shares."""
-        return TimeSeries(self.equity * self.investment / self.ts, index=self.ts.index)
+        return self.equity * self.investment / self.ts
 
     @property
     def returns(self):
         """Calculate returns at each time step."""
-        return TimeSeries(pct_change(self.equity), index=self.ts.index)
-
-    @property
-    def position_returns(self):
-        """Calculate returns on positions."""
-        nonzero_idxs = np.flatnonzero(self.positions)
-        position_equity = (np.abs(self.positions) * self.equity)[nonzero_idxs]
-        position_returns = pct_change(np.insert(position_equity, 0, 1))[1:]
-        return TimeSeries(position_returns, index=self.ts.index[nonzero_idxs])
+        return pct_change(self.equity)
