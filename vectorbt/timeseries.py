5d47f973e5bd4093f5c91cc5ae3d491b3d23244f
Oleg Polakow <olegpolakow@gmail.com>
2020-02-04 22:52:06 +0100

    TimeSeries class

--- vectorbt/timeseries.py
+++ vectorbt/timeseries.py
@@ -1,27 +1,243 @@
-from vectorbt.utils.decorators import to_dim1, has_type
-from vectorbt.utils.array import Array2D
+from vectorbt.utils.decorators import *
 import numpy as np
 import pandas as pd
+import inspect
+import sys
+from numba import njit, f8, i8, b1, optional
 from matplotlib import pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
+# ############# Numba functions ############# #
 
-class TimeSeries(Array2D):
+# All functions below require input to be a two-dimensional NumPy array,
+# where first axis is index and second axis are columns.
+# They all move along the first axis (axis=0)
+
+
+@njit(f8[:, :](f8[:, :], b1[:, :], f8))
+def set_by_mask_nb(a, mask, value):
+    """Set value by 2D boolean mask."""
+    b = a.copy()
+    for j in range(b.shape[1]):
+        b[mask[:, j], j] = value
+    return b
+
+
+@njit(f8[:, :](f8[:, :], f8))
+def fillna_nb(a, fill_value):
+    """Fill NaNs with fill_value."""
+    return set_by_mask_nb(a, np.isnan(a), fill_value)
+
+
+@njit(f8[:, :](f8[:, :], i8, f8))
+def prepend_nb(a, n, fill_value):
+    """Prepend n values to the array."""
+    b = np.full((a.shape[0]+n, a.shape[1]), fill_value, dtype=a.dtype)
+    b[-a.shape[0]:] = a
+    return b
+
+
+@njit(f8[:, :](f8[:, :], i8))
+def fshift_nb(a, n):
+    """Shift forward by n."""
+    a = prepend_nb(a, n, np.nan)
+    return a[:-n, :]
+
+
+@njit(f8[:, :](f8[:, :]))
+def pct_change_nb(a):
+    """Compute the percentage change from the immediately previous row."""
+    b = np.full_like(a, np.nan)
+    for i in range(a.shape[1]):
+        b[1:, i] = np.diff(a[:, i].copy()) / a[:-1, i]
+    return b
+
+
+@njit(f8[:, :](f8[:, :]))
+def ffill_nb(a):
+    """Fill NaNs with the last value."""
+    b = np.empty_like(a)
+    for j in range(a.shape[1]):
+        maxval = a[0, j]
+        for i in range(a.shape[0]):
+            if np.isnan(a[i, j]):
+                b[i, j] = maxval
+            else:
+                b[i, j] = a[i, j]
+                maxval = b[i, j]
+    return b
+
+
+@njit(f8[:, :, :](f8[:, :], i8))
+def _rolling_window_nb(a, window):
+    """Rolling window over the array.
+
+    Produce the pandas result when min_periods=1."""
+    a = prepend_nb(a, window-1, np.nan)
+    shape = (a.shape[0] - int(window) + 1, int(window)) + a.shape[1:]
+    strides = (a.strides[0],) + a.strides
+    strided = np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
+    return strided  # raw output
+
+
+@njit(f8[:, :](f8[:, :, :]))
+def _nanmean_nb(a):
+    # numba doesn't support axis kwarg in np.nan_ functions
+    b = np.empty((a.shape[0], a.shape[2]), dtype=a.dtype)
+    for j in range(a.shape[2]):
+        for i in range(a.shape[0]):
+            b[i, j] = np.nanmean(a[i, :, j])
+    return b
+
+
+@njit(f8[:, :](f8[:, :, :]))
+def _nanstd_nb(a):
+    # numba doesn't support axis kwarg in np.nan_ functions
+    b = np.empty((a.shape[0], a.shape[2]), dtype=a.dtype)
+    for j in range(a.shape[2]):
+        for i in range(a.shape[0]):
+            b[i, j] = np.nanstd(a[i, :, j])
+    return b
+
+
+@njit(f8[:, :](f8[:, :, :]))
+def _nanmax_nb(a):
+    # numba doesn't support axis kwarg in np.nan_ functions
+    b = np.empty((a.shape[0], a.shape[2]), dtype=a.dtype)
+    for j in range(a.shape[2]):
+        for i in range(a.shape[0]):
+            b[i, j] = np.nanmax(a[i, :, j])
+    return b
+
+
+@njit(f8[:, :](f8[:, :], optional(i8)))
+def rolling_mean_nb(a, window):
+    if window is None:  # expanding
+        window = a.shape[0]
+    return _nanmean_nb(_rolling_window_nb(a, window))
+
+
+@njit(f8[:, :](f8[:, :], optional(i8)))
+def rolling_std_nb(a, window):
+    if window is None:  # expanding
+        window = a.shape[0]
+    return _nanstd_nb(_rolling_window_nb(a, window))
+
+
+@njit(f8[:, :](f8[:, :], optional(i8)))
+def rolling_max_nb(a, window):
+    if window is None:  # expanding
+        window = a.shape[0]
+    return _nanmax_nb(_rolling_window_nb(a, window))
+
+
+@njit(f8[:](f8[:], i8))
+def _ewma_nb(arr_in, window):
+    # https://stackoverflow.com/a/51392341/8141780
+    n = arr_in.shape[0]
+    ewma = np.empty(n, dtype=arr_in.dtype)
+    alpha = 2 / float(window + 1)
+    w = 1
+    ewma_old = arr_in[0]
+    ewma[0] = ewma_old
+    for i in range(1, n):
+        w += (1-alpha)**i
+        ewma_old = ewma_old*(1-alpha) + arr_in[i]
+        ewma[i] = ewma_old / w
+    return ewma
+
+
+@njit(f8[:](f8[:], i8))
+def _ewma_infinite_hist_nb(arr_in, window):
+    # https://stackoverflow.com/a/51392341/8141780
+    n = arr_in.shape[0]
+    ewma = np.empty(n, dtype=arr_in.dtype)
+    alpha = 2 / float(window + 1)
+    ewma[0] = arr_in[0]
+    for i in range(1, n):
+        ewma[i] = arr_in[i] * alpha + ewma[i-1] * (1 - alpha)
+    return ewma
+
+
+@njit(f8[:, :](f8[:, :], optional(i8), b1))
+def ewma_nb(a, window, adjust):
+    """Exponential weighted moving average."""
+    b = np.empty_like(a)
+    if window is None:
+        window = a.shape[0]  # expanding
+    for i in range(a.shape[1]):
+        if adjust:
+            b[:, i] = _ewma_nb(a[:, i], window)
+        else:
+            b[:, i] = _ewma_infinite_hist_nb(a[:, i], window)
+    return b
+
+
+@njit(f8[:, :](f8[:, :]))
+def cumsum_nb(a):
+    """Cumulative sum (axis=0)."""
+    b = np.empty_like(a, dtype=a.dtype)
+    for j in range(a.shape[1]):
+        b[:, j] = np.cumsum(a[:, j])
+    return b
+
+
+@njit(f8[:, :](f8[:, :]))
+def cumprod_nb(a):
+    """Cumulative product (axis=0)."""
+    b = np.empty_like(a, dtype=a.dtype)
+    for j in range(a.shape[1]):
+        b[:, j] = np.cumprod(a[:, j])
+    return b
+
+# ############# Main class ############# #
+
+
+# List numba functions in current module
+nb_funcs = [
+    obj
+    for name, obj in inspect.getmembers(sys.modules[__name__])
+    if '_nb' in name and not name.startswith('_')
+]
+
+# Add numba functions as methods to the TimeSeries class
+@add_nb_methods(*nb_funcs)
+class TimeSeries(np.ndarray):
+    """Base class inherited by TimeSeries, Signals and Positions.
+    Similar to pd.DataFrame, but optimized for complex matrix operations.
+    NOTE: There is no index nor columns vars - you will have to handle them separately."""
+
     def __new__(cls, input_array):
-        obj = Array2D(input_array).view(cls)
-        if obj.dtype != np.float:
-            raise TypeError("dtype must be float")
+        # Input array is an already formed ndarray instance
+        # We first cast to be our class type
+        obj = np.asarray(input_array).view(cls)
+        if obj.ndim == 1:
+            obj = obj[:, None]  # expand
+        if obj.ndim != 2:
+            raise ValueError("Argument input_array must be a two-dimensional array")
+        if obj.dtype != np.float64:
+            raise TypeError("dtype must be np.float64")
+
+        # Be aware: index and columns do not change when slicing, subscribing or modifying the array!
+        # Since arrays are meant to be the same shape throught processing.
+        # If you want to change these variables as well, do it explicitly in your code.
+
+        # Finally, we must return the newly created object:
         return obj
 
     @classmethod
-    def empty(cls, shape):
-        """Create and fill an empty array with 0."""
-        return super().empty(shape, 0)
+    def full(cls, *args, **kwargs):
+        return cls(np.full(*args, **kwargs))
 
-    @to_dim1(0)
+    @classmethod
+    def full_like(cls, *args, **kwargs):
+        return cls(np.full_like(*args, **kwargs))
+
+    @to_dim1('self')
+    @to_dim1('benchmark', allow_number=True)
     def plot(self, index=None, label=None, benchmark=None, benchmark_label=None, ax=None, **kwargs):
-        """Plot time series as a line."""
+        """Plot TimeSeries as a line."""
         no_ax = ax is None
         if no_ax:
             fig, ax = plt.subplots()
@@ -38,15 +254,12 @@ class TimeSeries(Array2D):
 
         # Plot benchmark
         if benchmark is not None:
-            if isinstance(benchmark, pd.Series):
-                benchmark_df = benchmark
-                benchmark_df.columns = ['Benchmark']
-            elif isinstance(benchmark, (int, float, complex)):
+            if isinstance(benchmark, (int, float, complex)):
                 benchmark_df = pd.DataFrame(np.full(len(ts_df.index), benchmark))
                 benchmark_df.columns = [str(benchmark)]
                 benchmark_df.index = ts_df.index
             else:
-                benchmark_df = pd.DataFrame(benchmark)
+                benchmark_df = pd.DataFrame(np.asarray(benchmark))
                 benchmark_df.columns = ['Benchmark']
                 benchmark_df.index = ts_df.index
             if benchmark_label is not None:
