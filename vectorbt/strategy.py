f87ecb9c38c78bc2dba023bbff7fd32939c5c4d3
Oleg Polakow <olegpolakow@gmail.com>
2020-02-16 21:32:28 +0100

    Generalize class methods for more flexibility

--- vectorbt/strategy.py
+++ vectorbt/strategy.py
@@ -4,7 +4,7 @@ from numba import njit
 from numba.types import UniTuple, DictType, f8, i8, b1
 from numba.typed import Dict
 from vectorbt.timeseries import ewma_nb, rolling_mean_nb, rolling_std_nb, diff_nb, \
-set_by_mask_nb, fillna_nb, prepend_nb, rolling_max_nb, pct_change_nb, ffill_nb
+    set_by_mask_nb, fillna_nb, prepend_nb, rolling_max_nb, pct_change_nb, ffill_nb
 
 from vectorbt.utils.decorators import *
 from vectorbt.signals import Signals, generate_exits_nb
@@ -16,7 +16,7 @@ float_2d_array = f8[:, :]
 
 
 @njit(UniTuple(f8[:, :], 2)(f8[:, :], i8[:], i8[:], b1, b1, b1), cache=True)
-def ma_nb(ts, fast_windows, slow_windows, ewm, adjust, min_periods):
+def dmac_nb(ts, fast_windows, slow_windows, ewm, adjust, min_periods):
     """For each fast and slow window, calculate the corresponding SMA/EMA."""
     # Cache moving averages to effectively reduce the number of operations
     unique_windows = np.unique(np.concatenate((fast_windows, slow_windows)))
@@ -41,32 +41,34 @@ def ma_nb(ts, fast_windows, slow_windows, ewm, adjust, min_periods):
     return fast_mas, slow_mas
 
 
-class MovingAverage():
-    """The SMA is a technical indicator for determining if an asset price 
-    will continue or reverse a bull or bear trend. The SMA is calculated as 
-    the arithmetic average of an asset's price over some period.
+class DMAC():
+    """The Dual Moving Average Crossover trading system uses two moving averages, 
+    one short and one long. The system trades when the short moving average 
+    crosses the long moving average."""
 
-    The EMA is a moving average that places a greater weight and 
-    significance on the most recent data points."""
-
-    @has_type('ts', TimeSeries)
     @to_2d('ts')
     @to_1d('fast_windows')
     @to_1d('slow_windows')
     @broadcast('fast_windows', 'slow_windows')
     @has_dtype('fast_windows', np.int64)
     @has_dtype('slow_windows', np.int64)
+    @has_type('ts', TimeSeries)
     def __init__(self, ts, fast_windows, slow_windows, ewm=False, adjust=False, min_periods=True):
         # fast_windows and slow_windows can be either np.ndarray or single number
-        fast_mas, slow_mas = ma_nb(ts, fast_windows, slow_windows, ewm, adjust, min_periods)
-        self.fast_mas = TimeSeries(fast_mas)
-        self.slow_mas = TimeSeries(slow_mas)
+        fast, slow = dmac_nb(ts, fast_windows, slow_windows, ewm, adjust, min_periods)
+        self.fast = TimeSeries(fast)
+        self.slow = TimeSeries(slow)
 
-    def generate_entries(self):
-        return Signals(self.fast_mas > self.slow_mas)
+    def is_fast_above_slow(self):
+        return Signals(self.fast > self.slow)
 
-    def generate_exits(self):
-        return Signals(self.fast_mas < self.slow_mas)
+    def is_fast_below_slow(self):
+        return Signals(self.fast < self.slow)
+
+    def crossover_signals(self):
+        entries = self.is_fast_above_slow().first_true(after_false=True) # crossover is first true after false
+        exits = self.is_fast_below_slow().first_true(after_false=True)
+        return entries, exits
 
 # ############# BollingerBands ############# #
 
@@ -90,51 +92,65 @@ def bb_nb(ts, ns, ks, min_periods):
             mstd[:ns[i], :] = np.nan
         cache_d[ns[i]] = ma, mstd
     # Calculate lower, middle and upper bands
-    upper_bands = np.empty((ts.shape[0], ts.shape[1] * ns.shape[0]), dtype=f8)
-    middle_bands = np.empty((ts.shape[0], ts.shape[1] * ns.shape[0]), dtype=f8)
-    lower_bands = np.empty((ts.shape[0], ts.shape[1] * ns.shape[0]), dtype=f8)
+    upper = np.empty((ts.shape[0], ts.shape[1] * ns.shape[0]), dtype=f8)
+    middle = np.empty((ts.shape[0], ts.shape[1] * ns.shape[0]), dtype=f8)
+    lower = np.empty((ts.shape[0], ts.shape[1] * ns.shape[0]), dtype=f8)
     for i in range(ns.shape[0]):
         ma, mstd = cache_d[ns[i]]
-        upper_bands[:, i*ts.shape[1]:(i+1)*ts.shape[1]] = ma + ks[i] * mstd  # (MA + Kσ)
-        middle_bands[:, i*ts.shape[1]:(i+1)*ts.shape[1]] = ma  # MA
-        lower_bands[:, i*ts.shape[1]:(i+1)*ts.shape[1]] = ma - ks[i] * mstd  # (MA - Kσ)
-    return upper_bands, middle_bands, lower_bands
+        upper[:, i*ts.shape[1]:(i+1)*ts.shape[1]] = ma + ks[i] * mstd  # (MA + Kσ)
+        middle[:, i*ts.shape[1]:(i+1)*ts.shape[1]] = ma  # MA
+        lower[:, i*ts.shape[1]:(i+1)*ts.shape[1]] = ma - ks[i] * mstd  # (MA - Kσ)
+    return upper, middle, lower
 
 
 class BollingerBands():
     """Bollinger Bands® are volatility bands placed above and below a moving average."""
 
-    @has_type('ts', TimeSeries)
     @to_2d('ts')
     @to_1d('windows')
     @to_1d('std_ns')
     @broadcast('windows', 'std_ns')
     @has_dtype('windows', np.int64)
     @has_dtype('std_ns', np.int64)
+    @has_type('ts', TimeSeries)
     def __init__(self, ts, windows, std_ns, min_periods=True):
         # windows and std_ns can be either np.ndarray or single number
         self.ts = np.tile(ts, (1, windows.shape[0]))
-        upper_bands, middle_bands, lower_bands = bb_nb(ts, windows, std_ns, min_periods)
-        self.upper_bands = TimeSeries(upper_bands)
-        self.middle_bands = TimeSeries(middle_bands)
-        self.lower_bands = TimeSeries(lower_bands)
+        upper, middle, lower = bb_nb(ts, windows, std_ns, min_periods)
+        self.upper = TimeSeries(upper)
+        self.middle = TimeSeries(middle)
+        self.lower = TimeSeries(lower)
 
     @property
     def percent_b(self):
         """Shows where price is in relation to the bands.
         %b equals 1 at the upper band and 0 at the lower band."""
-        return TimeSeries((self.ts - self.lower_bands) / (self.upper_bands - self.lower_bands))
+        return TimeSeries((self.ts - self.lower) / (self.upper - self.lower))
 
     @property
     def bandwidth(self):
         """Bandwidth tells how wide the Bollinger Bands are on a normalized basis."""
-        return TimeSeries((self.upper_bands - self.lower_bands) / self.middle_bands)
+        return TimeSeries((self.upper - self.lower) / self.middle)
 
-    def generate_entries(self):
-        return Signals(self.ts >= self.upper_bands)
+    @to_2d('threshold')
+    @broadcast_to('threshold', 'self.ts')
+    def is_percent_b_above_threshold(self, threshold):
+        return Signals(self.percent_b > threshold)
 
-    def generate_exits(self):
-        return Signals(self.ts <= self.lower_bands)
+    @to_2d('threshold')
+    @broadcast_to('threshold', 'self.ts')
+    def is_percent_b_below_threshold(self, threshold):
+        return Signals(self.percent_b < threshold)
+
+    @to_2d('threshold')
+    @broadcast_to('threshold', 'self.ts')
+    def is_bandwidth_above_threshold(self, threshold):
+        return Signals(self.bandwidth > threshold)
+
+    @to_2d('threshold')
+    @broadcast_to('threshold', 'self.ts')
+    def is_bandwidth_below_threshold(self, threshold):
+        return Signals(self.bandwidth < threshold)
 
 
 # ############# RSI ############# #
@@ -178,22 +194,22 @@ class RSI():
     measures the magnitude of recent price changes to evaluate overbought 
     or oversold conditions in the price of a stock or other asset."""
 
-    @has_type('ts', TimeSeries)
     @to_2d('ts')
     @to_1d('windows')
     @has_dtype('windows', np.int64)
+    @has_type('ts', TimeSeries)
     def __init__(self, ts, windows, ewm=False, adjust=False, min_periods=True):
         self.rsi = TimeSeries(rsi_nb(ts, windows, ewm, adjust, min_periods))
 
-    @to_2d('lower_bound')
-    @broadcast_to('lower_bound', 'self.rsi')
-    def generate_entries(self, lower_bound):
-        return Signals(self.rsi < lower_bound)
+    @to_2d('threshold')
+    @broadcast_to('threshold', 'self.rsi')
+    def is_rsi_above_threshold(self, threshold):
+        return Signals(self.rsi > threshold)
 
-    @to_2d('upper_bound')
-    @broadcast_to('upper_bound', 'self.rsi')
-    def generate_exits(self, upper_bound):
-        return Signals(self.rsi > upper_bound)
+    @to_2d('threshold')
+    @broadcast_to('threshold', 'self.rsi')
+    def is_rsi_below_threshold(self, threshold):
+        return Signals(self.rsi < threshold)
 
 
 # ############# Risk minimization ############# #
@@ -212,7 +228,7 @@ def stoploss_exit_mask_nb(entries, col_idx, entry_idx, ts, stop, is_relative):
 @njit(b1[:, :](f8[:, :], b1[:, :], f8[:, :, :], b1, b1), cache=True)
 def stoploss_exits_nb(ts, entries, stops, is_relative, only_first):
     """Calculate exit signals based on stop loss strategy.
-    
+
     An approach here significantly differs from the approach with rolling windows.
     If user wants to try out different rolling windows, he can pass them as a 1d array.
     Here, user must be able to try different stops not only for the `ts` itself,
@@ -228,14 +244,14 @@ def stoploss_exits_nb(ts, entries, stops, is_relative, only_first):
     return exits
 
 
-@has_type('ts', TimeSeries)
-@has_type('entries', Signals)
 @to_2d('ts')
 @to_2d('entries')
 @broadcast('ts', 'entries')
 @broadcast_to_cube_of('stops', 'ts')
+@has_type('ts', TimeSeries)
+@has_type('entries', Signals)
 # stops can be either a number, an array of numbers, or an array of matrices each of ts shape
-def generate_stoploss_exits(ts, entries, stops, is_relative=True, only_first=True):
+def price_hits_stop_loss(ts, entries, stops, is_relative=True, only_first=True):
     """A stop-loss is designed to limit an investor's loss on a security position. 
     Setting a stop-loss order for 10% below the price at which you bought the stock 
     will limit your loss to 10%."""
@@ -246,7 +262,7 @@ def generate_stoploss_exits(ts, entries, stops, is_relative=True, only_first=Tru
 @njit(b1[:](b1[:, :], i8, i8, f8[:, :], f8[:, :], b1), cache=True)
 def tstop_exit_mask_nb(entries, col_idx, entry_idx, ts, stop, is_relative):
     """Index of the first event below the trailing stop."""
-    ts = np.expand_dims(ts[:, col_idx], axis=1) # most nb function perform on 2d data only
+    ts = np.expand_dims(ts[:, col_idx], axis=1)  # most nb function perform on 2d data only
     stop = np.expand_dims(stop[:, col_idx], axis=1)
     peak = np.full(ts.shape, np.nan)
     # Propagate the maximum value from the entry using expanding max
@@ -272,14 +288,14 @@ def tstop_exits_nb(ts, entries, stops, is_relative, only_first):
     return exits
 
 
-@has_type('ts', TimeSeries)
-@has_type('entries', Signals)
 @to_2d('ts')
 @to_2d('entries')
 @broadcast('ts', 'entries')
 @broadcast_to_cube_of('stops', 'ts')
+@has_type('ts', TimeSeries)
+@has_type('entries', Signals)
 # stops can be either a number, an array of numbers, or an array of matrices each of ts shape
-def generate_tstop_exits(ts, entries, stops, is_relative=True, only_first=True):
+def price_hits_trailing_stop(ts, entries, stops, is_relative=True, only_first=True):
     """A Trailing Stop order is a stop order that can be set at a defined percentage 
     or amount away from the current market price. The main difference between a regular 
     stop loss and a trailing stop is that the trailing stop moves as the price moves."""
