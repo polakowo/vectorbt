e17ac254c2616c33286953e3ca5e086082c5cc62
Oleg Polakow <olegpolakow@gmail.com>
2020-02-16 21:33:20 +0100

    Make metrics more robust to NaNs

--- vectorbt/portfolio.py
+++ vectorbt/portfolio.py
@@ -11,7 +11,7 @@ from vectorbt.positions import Positions
 
 @njit(f8[:, :](f8[:, :], i1[:, :], f8, f8[:, :], f8[:, :]), cache=True)
 def equity_nb(ts, positions, investment, fees, slippage):
-    equity = np.zeros_like(ts)
+    equity = np.empty_like(ts)
     for j in range(ts.shape[1]):
         j_equity = investment
         bought = False
@@ -29,12 +29,12 @@ def equity_nb(ts, positions, investment, fees, slippage):
 
 @njit(f8[:, :](f8[:, :], i1[:, :]), cache=True)
 def trade_profits_nb(equity, positions):
-    trade_profits = np.zeros_like(equity)
+    trade_profits = np.full_like(equity, np.nan)
     for j in range(equity.shape[1]):
         bought_i = -1
         bought = False
         for i in range(equity.shape[0]):
-            if bought and positions[i, j] == -1:
+            if bought and (positions[i, j] == -1 or i == equity.shape[0] - 1):
                 trade_profits[i, j] = equity[i, j] - equity[bought_i, j]
                 bought = False
             elif ~bought and positions[i, j] == 1:
@@ -45,12 +45,12 @@ def trade_profits_nb(equity, positions):
 
 @njit(f8[:, :](f8[:, :], i1[:, :]), cache=True)
 def trade_returns_nb(equity, positions):
-    trade_returns = np.zeros_like(equity)
+    trade_returns = np.full_like(equity, np.nan)
     for j in range(equity.shape[1]):
         bought_i = -1
         bought = False
         for i in range(equity.shape[0]):
-            if bought and positions[i, j] == -1:
+            if bought and (positions[i, j] == -1 or i == equity.shape[0] - 1):
                 trade_returns[i, j] = equity[i, j] / equity[bought_i, j] - 1
                 bought = False
             elif ~bought and positions[i, j] == 1:
@@ -64,13 +64,13 @@ def trade_returns_nb(equity, positions):
 class Portfolio():
     """Propagate investment through positions to generate equity and returns."""
 
-    @has_type('ts', TimeSeries)
-    @has_type('positions', Positions)
     @broadcast('ts', 'positions')
     @broadcast_to('fees', 'ts')
     @broadcast_to('slippage', 'ts')
     @to_dtype('fees', np.float64)
     @to_dtype('slippage', np.float64)
+    @has_type('ts', TimeSeries)
+    @has_type('positions', Positions)
     def __init__(self, ts, positions, investment=1., fees=0., slippage=0.):
         self.ts = ts
         self.positions = positions
@@ -107,6 +107,21 @@ class Portfolio():
 
     # ############# Performance metrics ############# #
 
+    @property
+    def key_metrics_df(self):
+        """Build dataframe with key metrics."""
+        df = pd.DataFrame(columns=range(self.ts.shape[1]))
+        props = ['total_net_profit', 'avg_win', 'avg_loss', 'win_prob', 'loss_prob', 'appt', 'mdd']
+        for prop in props:
+            df.loc[prop, :] = getattr(self, prop)
+        return df
+
+    def __repr__(self):
+        return self.key_metrics_df.__repr__()
+
+    def __str__(self):
+        return self.key_metrics_df.__str__()
+
     def reduce_win(self, func):
         """Perform reducing operation on wins."""
         trade_profits = self.trade_profits.copy()
@@ -121,19 +136,27 @@ class Portfolio():
 
     @property
     def sum_win(self):
-        return self.reduce_win(lambda x: np.nansum(x, axis=0))
+        sum_win = self.reduce_win(lambda x: np.nansum(x, axis=0))
+        sum_win[np.isnan(sum_win)] = 0
+        return sum_win
 
     @property
     def sum_loss(self):
-        return self.reduce_loss(lambda x: np.nansum(x, axis=0))
+        sum_loss = self.reduce_loss(lambda x: np.nansum(x, axis=0))
+        sum_loss[np.isnan(sum_loss)] = 0
+        return sum_loss
 
     @property
     def avg_win(self):
-        return self.reduce_win(lambda x: np.nanmean(x, axis=0))
+        avg_win = self.reduce_win(lambda x: np.nanmean(x, axis=0))
+        avg_win[np.isnan(avg_win)] = 0
+        return avg_win
 
     @property
     def avg_loss(self):
-        return self.reduce_loss(lambda x: np.nanmean(x, axis=0))
+        avg_loss = self.reduce_loss(lambda x: np.nanmean(x, axis=0))
+        avg_loss[np.isnan(avg_loss)] = 0
+        return avg_loss
 
     @property
     def win_prob(self):
