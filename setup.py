f2ad166e2b76d750a9c9d77e6069e0b1445b6ff7
Oleg Polakow <olegpolakow@gmail.com>
2020-02-19 01:34:05 +0100

    Update configuration

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from vectorbt import __version__
 setup(
     name='vectorbt',
     version=__version__,
-    description='A tiny, fully vectorized Python library for backtesting trading strategies at scale',
+    description='Python library for backtesting trading strategies at scale',
     author='Oleg Polakow',
     author_email='olegpolakow@gmail.com',
     url='https://github.com/polakowo/vectorbt',
