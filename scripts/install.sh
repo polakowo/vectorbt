ac17f249bdb8e8b27a19be0c9ce1f4b7b59184f3
Oleg Polakow <olegpolakow@gmail.com>
2021-04-03 17:04:21 +0200

    Fix deployment

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+curl -O https://netcologne.dl.sourceforge.net/project/ta-lib/ta-lib/0.4.0/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+"./configure --prefix=/usr"
+make
+sudo make install
+cd ..
+pip install .[cov,docs] empyrical
+pip install ray || true
+pip install ta || true
+pip install pandas_ta || true
+pip install TA-Lib || true
