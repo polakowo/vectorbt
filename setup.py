70052411a86a043f8905a07606d27c57214cf645
Oleg Polakow <olegpolakow@gmail.com>
2021-05-02 21:23:24 +0200

    Update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='vectorbt',
-    version='0.17.7',
+    version='0.18.0',
     description='Python library for backtesting and analyzing trading strategies at scale',
     author='Oleg Polakow',
     author_email='olegpolakow@gmail.com',
