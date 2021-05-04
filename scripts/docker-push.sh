1b2f7a3602cc24b91080cd68ead2aef385e493d5
Oleg Polakow <olegpolakow@gmail.com>
2021-05-04 20:30:56 +0200

    Fix docker-push.sh [ci deploy-docker]

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-docker tag "${1}" "${1}:${2}"
-docker tag "${1}" "${1}:latest"
-docker push "${1}" --all-tags
+TARGET_IMAGE_TAGGED="${1}:${2}"
+TARGET_IMAGE_LATEST="${1}:latest"
+
+docker tag "${1}" "${TARGET_IMAGE_TAGGED}"
+docker push "${TARGET_IMAGE_TAGGED}"
+
+docker tag "${1}" "${TARGET_IMAGE_LATEST}"
+docker push "${TARGET_IMAGE_LATEST}"
