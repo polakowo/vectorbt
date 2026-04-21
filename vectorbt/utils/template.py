45609cb6b1b6e7929ef618d2a3b4f974395f89e7
Oleg Polakow <olegpolakow@gmail.com>
2026-04-21 19:55:05 +0200

    chore: update copyright years to 2017-2026 across all source files

--- vectorbt/utils/template.py
+++ vectorbt/utils/template.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Oleg Polakow. All rights reserved.
+# Copyright (c) 2017-2026 Oleg Polakow. All rights reserved.
 # This code is licensed under Apache 2.0 with Commons Clause license (see LICENSE.md for details)
 
 """Utilities for working with templates."""
